cd6bc7d755188d51ab29000c5ff7e879f61544a0
Danilo Requena <danilo.requena26@gmail.com>
2020-04-19 18:34:03 -0300

    Initial Commit

---
+++ CustomTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomTimer
+//
+//  Created by Danilo Requena on 19/04/20.
+//  Copyright © 2020 Danilo Requena. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
